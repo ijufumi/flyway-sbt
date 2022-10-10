20ff708c04f9d56aabb523cae84cb0a7262f288b
Takafumi Iju <ijufumi@gmail.com>
2022-10-10 10:08:46 +0900

    Fixed build error.

--- src/sbt-test/flyway-sbt/test3/src/main/java/Hello.java
+++ src/sbt-test/flyway-sbt/test3/src/main/java/Hello.java
@@ -1,8 +1,8 @@
-import org.flywaydb.core.api.migration.BaseJavaMigration
-import org.flywaydb.core.api.migration.Context
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
 
 public class Hello extends BaseJavaMigration {
-	public void migrate(context: Context) {
+	public void migrate(context: Context) throws Exception {
 		throw new Exception("");
 	}
 }
